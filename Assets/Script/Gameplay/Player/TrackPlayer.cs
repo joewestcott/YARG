2379d805155f3a5dbb526946ea84beb2f20e479e
wyrdough <nathan@nwacg.net>
2026-04-01 00:10:36 -0400

    Unmute stem on BRE start

--- Assets/Script/Gameplay/Player/TrackPlayer.cs
+++ Assets/Script/Gameplay/Player/TrackPlayer.cs
@@ -1064,6 +1064,7 @@ namespace YARG.Gameplay.Player
         {
             CurrentCoda = coda;
             CurrentCoda.SetLaneIndexes(GetLaneIndexes());
+            SetStemMuteState(false);
             GameManager.StartCoda();
         }
 
